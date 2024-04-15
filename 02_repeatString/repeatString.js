fe9930265f89915913688abb2d722df0ed52b6a3
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-04-15 16:55:48 +0100

    finish 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    let array = [];
+    if (repeats >= 0) {
+        for (let i = 0; i < repeats; i++) {
+            array.push(string)
+        };
+        let newString = array.join('');
+        return newString; 
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
