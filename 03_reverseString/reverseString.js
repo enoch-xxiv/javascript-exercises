2fa7445d718c6c4db03abd5a3301bd03096bfe46
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-04-16 12:36:16 +0100

    finsih reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
+function reverseString(string) {
 
-};
+    letters = string.split("");
+    reversedLetters = letters.reverse();
+    reversedString = reversedLetters.join('');
+
+    return reversedString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
