35dd2aad2dc7fac1addd3bc708194693ca4f6723
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-04-25 14:13:32 +0100

    finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
-};
+function convertToCelsius (temp) {
+  let tempInCelcius = (temp - 32) * (5/9);
+    let roundedTemp = parseFloat(tempInCelcius.toFixed(1));
+    return roundedTemp;
+}
 
-const convertToFahrenheit = function() {
-};
+function convertToFahrenheit (temp) {
+  let tempInFahrenheit = temp * (9/5) + 32;
+  let roundedTemp = parseFloat(tempInFahrenheit.toFixed(1));
+  return roundedTemp;
+}
 
 // Do not edit below this line
 module.exports = {
