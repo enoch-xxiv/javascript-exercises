d3227fb4ed0c5c7d15bd4707d493b062702b1a3f
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-04-25 12:37:36 +0100

    finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+function sumAll (a, b) {
+    if (typeof a == 'number' && typeof b == 'number') {
+        if (a > 0 && b > 0) {
+            const start = Math.min(a,b);
+            const end = Math.max(a,b);
+            let sum = 0;
+            for (i = start; i <= end; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+        else {
+            return "ERROR";
+        }
+    }
+    else {
+        return "ERROR";
+    };
+}
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
