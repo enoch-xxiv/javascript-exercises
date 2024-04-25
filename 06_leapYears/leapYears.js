563ce640431c5e7f0dcaf5fd29c085375e3d0095
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-04-25 13:05:58 +0100

    finish 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,7 @@
-const leapYears = function() {
-
-};
-
+function leapYears (year) {
+    if (year % 100 == 0 && year % 400 !== 0) return false;
+    if (year % 4 == 0) return true;
+    return false;
+}
 // Do not edit below this line
 module.exports = leapYears;
